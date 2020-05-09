534bad153f6d7695e75a35dd862bba7d8aa414ec
guofeng201507 <guofeng20094@gmail.com>
2020-05-09 19:22:50 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,112 @@
+import os
+import pickle
+
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+import pandas as pd
+from stable_baselines import PPO2
+from stable_baselines.common.vec_env import DummyVecEnv
+
+from rlenv.StockTradingEnv_US import StockTradingEnv_US
+
+# from stable_baselines.deepq.policies import MlpPolicy
+
+font = fm.FontProperties(fname='font/wqy-microhei.ttc')
+# plt.rc('font', family='Source Han Sans CN')
+plt.rcParams['axes.unicode_minus'] = False
+
+INITIAL_ACCOUNT_BALANCE = 50000
+
+
+def find_file(path, name):
+    # print(path, name)
+    for root, dirs, files in os.walk(path):
+        for fname in files:
+            if name in fname:
+                return os.path.join(root, fname)
+
+
+def test_a_stock_trade_US(stock_code):
+    stock_file_train = find_file('./stockdata/train', str(stock_code))
+
+    daily_profits, buy_hold_profit = stock_trade_US(stock_file_train)
+    fig, ax = plt.subplots()
+    ax.plot(daily_profits, '-o', label='AI QQQ', marker='o', ms=10, alpha=0.7, mfc='orange')
+    ax.plot(buy_hold_profit, '-o', label='B&H QQQ', marker='o', ms=10, alpha=0.7, mfc='blue')
+    ax.grid()
+    plt.xlabel('step')
+    plt.ylabel('profit')
+    ax.legend(prop=font)
+    # plt.show()
+    plt.savefig(f'./img/{stock_code}.png')
+
+
+def multi_stock_trade():
+    start_code = 600000
+    max_num = 3000
+
+    group_result = []
+
+    for code in range(start_code, start_code + max_num):
+        stock_file = find_file('./stockdata/train', str(code))
+        if stock_file:
+            try:
+                profits = stock_trade_US(stock_file)
+                group_result.append(profits)
+            except Exception as err:
+                print(err)
+
+    with open(f'code-{start_code}-{start_code + max_num}.pkl', 'wb') as f:
+        pickle.dump(group_result, f)
+
+
+def stock_trade_US(stock_file_train):
+    df_train = pd.read_csv(stock_file_train)
+    # df_train = df_train.sort_values('date')
+
+    # The algorithms require a vectorized environment to run
+    env_train = DummyVecEnv([lambda: StockTradingEnv_US(df_train)])
+
+    model = PPO2('MlpPolicy', env_train, verbose=0, tensorboard_log='./log').learn(total_timesteps=int(1e4))
+
+    # Random Agent, after training
+    # mean_reward, std_reward = evaluate_policy(model, env_train, n_eval_episodes=100)
+    # print(f"after training, mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+
+    # -----------------Test Model --------------------------------------
+    day_profits = []
+    buy_hold_profit = []
+
+    df_test = pd.read_csv(stock_file_train.replace('train', 'test'))
+
+    env_test = DummyVecEnv([lambda: StockTradingEnv_US(df_test)])
+    obs = env_test.reset()
+    no_of_shares = 0
+    buy_hold_commission = 0
+    for i in range(len(df_test) - 1):
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env_test.step(action)
+        profit = env_test.render()
+        day_profits.append(profit)
+        if i == 0:
+            buy_hold_profit.append(0)
+            no_of_shares = INITIAL_ACCOUNT_BALANCE // df_test.iloc[0]['Close']
+            buy_hold_commission = no_of_shares * df_test.iloc[0]['Close'] * 0.001
+            print('Buy ' + str(no_of_shares) + ' shares and hold')
+        else:
+            but_hold_profit_per_step = no_of_shares * (
+                    df_test.iloc[i]['Close'] - df_test.iloc[0]['Close']) - buy_hold_commission
+            buy_hold_profit.append(but_hold_profit_per_step)
+            print('Buy and Hold: ' + '*' * 40)
+            print('No of shares: ' + str(no_of_shares) + ' average cost per share ' + str(df_test.iloc[0]['Close']))
+            print('profit is ' + str(but_hold_profit_per_step))
+        if done:
+            break
+    return day_profits, buy_hold_profit
+
+
+if __name__ == '__main__':
+    # multi_stock_trade()
+    test_a_stock_trade_US('QQQ')
+    # ret = find_file('./stockdata/train', '600036')
+    # print(ret)
---
+++ rlenv/StockTradingEnv_US.py
@@ -0,0 +1,175 @@
+import gym
+import numpy as np
+from gym import spaces
+
+MAX_ACCOUNT_BALANCE = 2147483647
+MAX_NUM_SHARES = 2147483647
+MAX_SHARE_PRICE = 5000
+MAX_VOLUME = 1000e8
+MAX_AMOUNT = 3e10
+MAX_OPEN_POSITIONS = 5
+MAX_STEPS = 20000
+MAX_DAY_CHANGE = 1
+MIN_COMMISSION = 1
+
+INITIAL_ACCOUNT_BALANCE = 50000
+
+
+class StockTradingEnv_US(gym.Env):
+    """A stock trading environment for OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, df):
+        super(StockTradingEnv_US, self).__init__()
+
+        self.df = df
+        self.reward_range = (0, MAX_ACCOUNT_BALANCE)
+
+        # Actions of the format Buy x%, Sell x%, Hold, etc.
+        self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+
+        # Prices contains the OHCL values for the last five prices
+        self.observation_space = spaces.Box(low=0, high=1, shape=(14,), dtype=np.float16)
+
+    def _next_observation(self):
+        obs = np.array([
+            self.df.loc[self.current_step, 'Open'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'High'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'Low'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'Close'] / MAX_SHARE_PRICE,
+            self.df.loc[self.current_step, 'Volume'] / MAX_VOLUME,
+            self.balance / MAX_ACCOUNT_BALANCE,
+            self.max_net_worth / MAX_ACCOUNT_BALANCE,
+            self.commission_cost / MAX_ACCOUNT_BALANCE,
+            self.total_commission_cost / MAX_ACCOUNT_BALANCE,
+            self.shares_bought / MAX_NUM_SHARES,
+            self.shares_held / MAX_NUM_SHARES,
+            self.cost_basis / MAX_SHARE_PRICE,
+            self.shares_sold / MAX_NUM_SHARES,
+            self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
+        ])
+        return obs
+
+    def _take_action(self, action):
+        # Set the current price to a random price within the time step
+        # current_price = random.uniform(
+        #     self.df.loc[self.current_step, "open"], self.df.loc[self.current_step, "close"])
+
+        current_price = self.df.loc[self.current_step, "Close"]
+
+        action_type = action[0]
+        amount = action[1]
+        commission_cost = 0
+
+        if action_type < 1:
+            # Buy amount % of balance in shares
+            total_possible = int(self.balance / current_price)
+            shares_bought = int(total_possible * amount)
+            prev_cost = self.cost_basis * self.shares_held
+            additional_cost = shares_bought * current_price
+            if shares_bought > 0:
+                commission_cost = max(MIN_COMMISSION, additional_cost * 0.001)  # Assume 0.1 % trade commission fee
+            additional_cost += commission_cost
+
+            self.shares_bought = shares_bought
+            self.shares_sold = 0
+            self.commission_cost = commission_cost
+            self.total_commission_cost += commission_cost
+            self.balance -= additional_cost
+            self.cost_basis = (prev_cost + additional_cost) / (self.shares_held + shares_bought)
+            self.shares_held += shares_bought
+
+        elif action_type < 2:
+            # Sell amount % of shares held
+            shares_sold = int(self.shares_held * amount)
+            if shares_sold > 0:
+                commission_cost = max(MIN_COMMISSION,
+                                  shares_sold * current_price * 0.001)  # Assume 0.1 % trade commission fee. min 1 dollar
+            self.balance += shares_sold * current_price - commission_cost
+
+            self.commission_cost = commission_cost
+            self.total_commission_cost += commission_cost
+            self.shares_held -= shares_sold
+            self.shares_bought = 0
+            self.shares_sold = shares_sold
+            self.total_sales_value += shares_sold * current_price
+
+
+        self.net_worth = self.balance + self.shares_held * current_price
+
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+
+        if self.shares_held == 0:
+            self.cost_basis = 0
+
+    def step(self, action):
+        # Execute one time step within the environment
+        self._take_action(action)
+        done = False
+
+        self.current_step += 1
+
+        if self.current_step > len(self.df.loc[:, 'Open'].values) - 1:
+            self.current_step = 0  # loop training
+            # done = True
+
+        delay_modifier = (self.current_step / MAX_STEPS)
+
+        # profits
+        reward = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        if reward > 0:
+            reward = 1
+        elif reward == 0:
+            reward = -100
+        else:
+            reward = -100
+
+        if self.net_worth <= 0:
+            done = True
+
+        obs = self._next_observation()
+
+        return obs, reward, done, {}
+
+    def reset(self, new_df=None):
+        # Reset the state of the environment to an initial state
+        self.balance = INITIAL_ACCOUNT_BALANCE
+        self.net_worth = INITIAL_ACCOUNT_BALANCE
+        self.max_net_worth = INITIAL_ACCOUNT_BALANCE
+        self.shares_held = 0
+        self.cost_basis = 0
+        self.shares_bought = 0
+        self.shares_sold = 0
+        self.total_sales_value = 0
+        self.total_commission_cost = 0
+        self.commission_cost = 0
+
+        # pass test dataset to environment
+        if new_df:
+            self.df = new_df
+
+        # Set the current step to a random point within the data frame
+        # self.current_step = random.randint(
+        #     0, len(self.df.loc[:, 'open'].values) - 6)
+        self.current_step = 0
+
+        return self._next_observation()
+
+    def render(self, mode='human', close=False):
+        # Render the environment to the screen
+        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        print('-' * 30)
+        print(f'Step: {self.current_step}')
+        print(f'Balance: {self.balance}')
+
+        print(f'Shares bought : {self.shares_bought}')
+        print(f'Shares sold : {self.shares_sold}')
+        print(f'Shares held: {self.shares_held} ')
+
+        print(f'Total commission cost: {self.total_commission_cost}')
+        print(f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+
+        print(f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+        print(f'Profit: {profit}')
+        return profit
